1f4eadbf5f3e1d40a1ba343fdc6316bea0f15742
ParisaDarkhal <p.darkhal@gmail.com>
2023-08-26 22:38:54 -0400

    code edited

--- client/src/components/PhotoAlbumPage.js
+++ client/src/components/PhotoAlbumPage.js
@@ -48,9 +48,26 @@ const MyAlbum = ({
       const fetchImages = async () => {
         try {
           const response = await axios.get("/api/images");
-          const urls = response.data.map(
-            (imageName) => `http://localhost:3001/images/${imageName}`
-          );
+          const images = response.data.map((imgName) => {
+            const src = `http://localhost:3001/images/${imgName}`;
+            const img = new Image();
+            img.src = src;
+            if (img.width > img.height) {
+              img.width = 400;
+              img.height = 300;
+            } else {
+              img.width = 300;
+              img.height = 400;
+            }
+            return img;
+          });
+
+          const urls = images.map((image) => ({
+            key: image.src,
+            src: image.src,
+            width: image.width,
+            height: image.height,
+          }));
           setImageUrls(urls);
           setNewUpload(false);
         } catch (error) {
@@ -66,6 +83,11 @@ const MyAlbum = ({
       <PhotoAlbum
         layout="masonry"
         photos={imageUrls}
+        style={{
+          transition: "transform 0.3s",
+          cursor: "pointer",
+          "&:hover": { transform: "scale(1.1)" },
+        }}
         onClick={({ index }) => setIndex(index)}
       />
     </>
